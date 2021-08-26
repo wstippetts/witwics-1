1ff3a72e6b508f7110f00035acb230fde500c75b
mark ohnsman <mark.ohnsman@gmail.com>
2021-08-26 16:42:55 -0600

    adds local storage

--- test/modules/clue1.test.js
+++ test/modules/clue1.test.js
@@ -10,49 +10,49 @@ describe("clue1", () => {
     })
   })
 
-  describe("Concat Function", () => {
-    let concat = window['concat']
-    it("Will join two strings with the default seperator", () => {
-      chai.assert.strictEqual(concat("Javascript", "is awesome!"), "Javascript is awesome!")
-    })
+  // describe("Concat Function", () => {
+  //   let concat = window['concat']
+  //   it("Will join two strings with the default seperator", () => {
+  //     chai.assert.strictEqual(concat("Javascript", "is awesome!"), "Javascript is awesome!")
+  //   })
 
-    it("Will accept the optional seperator", () => {
-      chai.assert.strictEqual(concat("JavaScript", "is Awesome!!!", ", "), "JavaScript, is Awesome!!!")
-    })
-  })
+  //   it("Will accept the optional seperator", () => {
+  //     chai.assert.strictEqual(concat("JavaScript", "is Awesome!!!", ", "), "JavaScript, is Awesome!!!")
+  //   })
+  // })
 
-  describe("Capitalizer Function", () => {
-    let capitalizer = window['capitalizer']
-    it("Returns a string", () => {
-      chai.assert.isString(capitalizer("string", 1))
-    })
+  // describe("Capitalizer Function", () => {
+  //   let capitalizer = window['capitalizer']
+  //   it("Returns a string", () => {
+  //     chai.assert.isString(capitalizer("string", 1))
+  //   })
 
-    it("Capitalizes the character at the target position", () => {
-      chai.assert.strictEqual(capitalizer("bananana", 2), "N")
-    })
+  //   it("Capitalizes the character at the target position", () => {
+  //     chai.assert.strictEqual(capitalizer("bananana", 2), "N")
+  //   })
 
-    it("Works with any length of string", () => {
-      chai.assert.strictEqual(capitalizer("How much wood could a woodchuck chuck if a woodchuck could chuck wood", 22), "W")
-    })
+  //   it("Works with any length of string", () => {
+  //     chai.assert.strictEqual(capitalizer("How much wood could a woodchuck chuck if a woodchuck could chuck wood", 22), "W")
+  //   })
 
-  })
+  // })
 
-  describe("messageContainsPhrase Function", () => {
-    let messageContainsPhrase = window['messageContainsPhrase']
-    it("returns a boolean value", ()=>{
-      chai.assert.isBoolean(messageContainsPhrase('Hello World','o'), 'Be sure to return a bool value')
-    })
-    it("finds a single letter in a message", ()=>{
-      chai.assert.isTrue(messageContainsPhrase('Hello World','o'), 'be sure to return true with the the message contains the the phrase')
-    })
-    it("finds a phrase in a message", ()=>{
-      chai.assert.isTrue(messageContainsPhrase('It\'s dangerous to go alone, take this.','go alone'), 'should work on phrases including spaces')
-    })
-    it("returns false if phrase not found", ()=>{
-      chai.assert.isFalse(messageContainsPhrase('It\'s dangerous to go alone, take this.','zelda'), 'should return false if the phrase does not exist in the message')
-    })
-    it("is not case sensitive", ()=>{
-      chai.assert.isFalse(messageContainsPhrase('Welcome to Jurassic Park', 'jurassic'), 'should return true if the phrase is in the message despite casing')
-    })
-  })
+  // describe("messageContainsPhrase Function", () => {
+  //   let messageContainsPhrase = window['messageContainsPhrase']
+  //   it("returns a boolean value", ()=>{
+  //     chai.assert.isBoolean(messageContainsPhrase('Hello World','o'), 'Be sure to return a bool value')
+  //   })
+  //   it("finds a single letter in a message", ()=>{
+  //     chai.assert.isTrue(messageContainsPhrase('Hello World','o'), 'be sure to return true with the the message contains the the phrase')
+  //   })
+  //   it("finds a phrase in a message", ()=>{
+  //     chai.assert.isTrue(messageContainsPhrase('It\'s dangerous to go alone, take this.','go alone'), 'should work on phrases including spaces')
+  //   })
+  //   it("returns false if phrase not found", ()=>{
+  //     chai.assert.isFalse(messageContainsPhrase('It\'s dangerous to go alone, take this.','zelda'), 'should return false if the phrase does not exist in the message')
+  //   })
+  //   it("is not case sensitive", ()=>{
+  //     chai.assert.isFalse(messageContainsPhrase('Welcome to Jurassic Park', 'jurassic'), 'should return true if the phrase is in the message despite casing')
+  //   })
+  // })
 })
--- test/runner.js
+++ test/runner.js
@@ -8,15 +8,24 @@ import './modules/clue5.test.js'
 
 const runner = mocha.run()
 
-runner.on("suite end", (suite)=>{
+runner.on("suite end", async(suite)=>{
+  let solution = JSON.parse(localStorage.getItem('witwics1')) || {}
   if(suite.suites.length > 0 ){
+    solution[suite.title] = solution[suite.title] || { }
+    soulution[suite.title].completed = false
     const allPass = suite.suites.every(s => {
       return s.tests.length > 0 ? suiteIsPassing(s) : false
     })
+    
     if(allPass){
-      console.log(suite.title,'suite done')
-      // call api here
+      if(!solution[suite.title].img){
+        let res = await axios.get('https://bcw-sandbox.herokuapp.com/api/witwics/1/'+suite.title)
+        solution[suite.title] = res.data
+      }
+      solution[suite.title].complete = true
     }
+
+    localStorage.setItem('witwics1', solution)
   }
 })
 
